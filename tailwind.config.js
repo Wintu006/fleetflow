e0e0f221fe01e8e07115d61f9c24357e28b1455f
Wytalo Matheus <94622513+Wintu006@users.noreply.github.com>
2026-05-08 18:31:28 -0300

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -90,5 +90,8 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/forms")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@tailwindcss/forms"),
+  ],
 }
